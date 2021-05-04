90b7bd541d20a2a398af8c35d5c8dc105cb037c3
Stanhoucke <stanhoucke@gmail.com>
2021-05-04 09:55:56 +0100

    can fetch all shoes from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Shop from './containers/Shop';
+import Request from './helpers/Request';
 
 function App() {
+  const [shoes, setShoes] = useState([]);
+
+  const request = new Request();
+
+  useEffect (() => {
+    getShoes();
+  }, [])
+
+  const getShoes = () => {
+    request.get("/api/shoes")
+    .then(data => setShoes(data))
+  }
+
   return (
     <div className="App">
       <h1>Golden Shoe</h1>
